dc40d44e0b4ae89ac505570162f4154259d090db
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-10-03 20:04:41 +0200

    Capitalize words

--- src/ch11/Exercises.hs
+++ src/ch11/Exercises.hs
@@ -1,5 +1,7 @@
 module Exercises where
 
+import Data.Char (toUpper)
+
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf [] _ = True
 isSubsequenceOf _ [] = False
@@ -7,3 +9,8 @@ isSubsequenceOf sub@(x:xs) (y:ys) =
   if x /= y
     then isSubsequenceOf sub ys
     else isSubsequenceOf xs ys
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords xs = [(x, capitalize x) | x <- words xs]
+  where
+    capitalize (x:xs) = toUpper x : xs
